f2d6d332654a529b075da973d8384607a83a15dc
glhrmarques <gmarques.pd@gmail.com>
2026-02-19 18:30:58 -0300

    ARS to BRL

--- db.js
+++ db.js
@@ -9,12 +9,19 @@ const PORT = 4040;
 
 app.get('/currencies', async (req,res) => {
 
-    const url = process.env.API_URL
+    const arsPeso = "ars"
+    const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${arsPeso}.json`
 
     try {
 
-        const response = await axios.get(url)
-        res.json(response.data);
+        const response = await axios.get(url);
+        const rate = response.data.ars.brl;
+
+        const amountInARS = 12000;
+        const amountInBRL = amountInARS * rate;
+
+        console.log(amountInBRL.toFixed(2));
+        res.json(response.data)
 
     } catch (error) {
         console.error(error)
