e43bda13b3b64fa68ba6c9acf42486d07e4fd421
glhrmarques <gmarques.pd@gmail.com>
2026-02-19 18:01:44 -0300

    get currencies

--- db.js
+++ db.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import axios from 'axios';
 
 dotenv.config()
 
 const app = express();
 const PORT = 4040;
 
-app.get('/', (req, res) => {
-    res.send("testing server");
-});
+app.get('/currencies', async (req,res) => {
+
+    const url = process.env.API_URL
+
+    try {
+
+        const response = await axios.get(url)
+        res.json(response.data);
+
+    } catch (error) {
+        console.error(error)
+    }
+
+})
 
 app.listen(PORT, console.log(`Server is running in http://localhost:${PORT}`));
