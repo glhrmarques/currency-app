34cd305ec662a3c407a612ce16c4ad07d0cdf01b
glhrmarques <gmarques.pd@gmail.com>
2026-02-21 12:48:16 -0300

    fast response

--- frontend/actualCurrency.js
+++ frontend/actualCurrency.js
@@ -3,8 +3,7 @@ const oneCurrency = document.querySelector("#actualCurrency");
 async function currentCurrency() {
 
     const res = await fetch('https://currency-app-backend.onrender.com/currencies/brl');
-    const data = await res.json();
-    const rate = data.brl.ars;
+    const rate = await res.json();
 
     const oneCurrency = document.querySelector("#actualCurrency");
     const amount = 1;
